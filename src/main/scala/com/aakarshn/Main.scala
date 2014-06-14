dfdf530e6ed9c4980bcdeef97f0b55b29216e4d0
Aakarsh Nair <aakarsh.nair@gmail.com>
2014-06-14 09:56:08 +0000

    start

---
+++ src/main/scala/com/aakarshn/Main.scala
@@ -0,0 +1,17 @@
+package com.aakarshn 
+
+import scala.util.parsing.combinator._
+import scala.io.Source
+import java.io._
+
+import com.aakarshn._
+
+/**
+  A direct port of the evaluator for simply typed lambda calculus.
+  */
+object Main {
+
+  def main(args:Array[String]): scala.Unit= {
+    println("noturing: Howdy sailor");
+  }
+}
